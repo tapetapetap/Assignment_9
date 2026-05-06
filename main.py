bd97d1cefc3b2bfac10c099079a655b0aa4c4178
tapetapetap <jiwon.seo@g.skku.edu>
2026-05-06 20:45:10 +0900

    work made by tapetapetap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from typing import List
 
+# Worked in collaboration with tapetapetap:
+
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
     # implemented as a one-liner
