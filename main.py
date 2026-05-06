ccc3583b4e6abfcaeb79bb94a35ccdd4073dbaaa
tapetapetap <jiwon.seo@g.skku.edu>
2026-05-06 20:20:55 +0900

    Fix: 파이썬 문법 에러 및 충돌 마커 제거

--- main.py
+++ main.py
@@ -2,18 +2,8 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-<<<<<<< HEAD
-<<<<<<< HEAD
-    # Will this be working?
-    lines = open(path, 'r').read().split('\n')
-=======
     # implemented as a one-liner
     lines = open(path, 'r').read().split('\n')
->>>>>>> path_to_file_list
-=======
-    # Will this be working?
-    lines = open(path, 'r').read().split('\n')
->>>>>>> train_file_list_to_json
     return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
@@ -22,7 +12,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     def process_file(file):
         if '\\' in file:
             file = file.replace('\\', '\\\\')
-        if '/' or '"' in file:
+        if '/' in file  or '"' in file:
             file = file.replace('/', '\\/')
             file = file.replace('"', '\\"')
         return file
